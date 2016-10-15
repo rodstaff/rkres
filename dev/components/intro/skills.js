292f8a0db3f14a43317d97a181e54947a0d9ae94
rodk <rod2010rod2010@hotmail.com>
2016-10-15 19:56:12 -0400

    modified skills.js

--- dev/components/intro/skills.js
+++ dev/components/intro/skills.js
@@ -6,9 +6,11 @@ export default class Skills extends React.Component {
       <div>
         <h2>SKILLS</h2>
         <ul>
-          <li>Programming Languages:  &nbsp;&nbsp;JavaScript, Ruby</li>
-          <li>Front-End Dev't:  &nbsp;&nbsp;ReactJS+Redux, JQuery, HTML, CSS.</li>
-          <li>Back-End Dev't:  &nbsp;&nbsp;NodeJS, Ruby on Rails, RESTful API's, UNIX, Git version management, Heroku deployment.</li>
+          <li>Programming Languages: &nbsp;&nbsp;JavaScript, Ruby.</li>
+          <li>Database: &nbsp;&nbsp;PostgreSQL(, MongoDB).</li>
+          <li>Front-End Dev't: &nbsp;&nbsp;ReactJS+Redux, JQuery, HTML, CSS.</li>
+          <li>Back-End Dev't: &nbsp;&nbsp;NodeJS, Ruby on Rails, API's, MVC, OOP, REST/CRUD.</li>
+          <li>Build Tools/Env: &nbsp;&nbsp;Webpack, Babel, Git source control, Heroku deployment, UNIX.</li>
         </ul>
       </div>
   	);
--- dev/components/work/ti.js
+++ dev/components/work/ti.js
@@ -6,11 +6,11 @@ export default class PS extends React.Component {
       <div>
         <h3><em>CIRCUIT DEVELOPER | DESIGN ENGINEER</em>, &nbsp;&nbsp;TEXAS INSTRUMENT (the global analog and digital semiconductor IC design manufacturing group), &nbsp;1989 - 1995</h3>
         <ul>
-          <li>Designed prototype analog power management devices using computer aided design (CAD), from concept-to-engineering-to-manufacture contributing to the generation of $600,000 in new sales.</li>
+          <li>Designed prototype analog power management devices using computer aided design (CAD) in a UNIX environment, from concept-to-engineering-to-manufacture contributing to the generation of $600,000 in new sales.</li>
           <li>Initiated and coordinated troubleshooting activities and failure root cause analysis with the testing, wafer fabrication and customer engineers providing optimum and novel solutions delivering products at market premium prices.</li>
           <li>Generated specifications for assigned projects forwarding them to the sales and marketing departments for bidding processes compiling summary reports detailing configuration and “trade-off” studies for “best/worst case” recommendations interacting with senior management.</li>
           <li>Introduced and encouraged the initial use of software modeling of motor driver key components moving from an otherwise error-prone trial-and-error method in analog board design.</li>
-          <li>Software Tools:  Pascal (Test/Debugging), SPICE (Circuit Simulation), Silicon Graphics (Layout Generator).</li>
+          <li>Software Tools:  Pascal (Test/Debugging), SPICE (Circuit Simulation), Silicon Graphics (Layout Generator), UNIX-based Workstation.</li>
         </ul>
       </div>
   	);
